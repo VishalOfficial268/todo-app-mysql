f14d003163608242159fc13b964b786e76ad50a1
AadiVishu4 <vishalofiicial268@gmail.com>
2022-07-25 22:26:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const PORT = 5100;
+
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send("Todo Rest API in MySql + JWT + NodeJs + Express");
+})
+app.listen(PORT, () => {
+    console.log(`server running at http://localhost:${PORT}`);
+})
